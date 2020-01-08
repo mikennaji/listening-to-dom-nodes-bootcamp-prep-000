3d9db8b4d0d438ba4a880cd4e73c02608570fa36
Michael Nnaji <mnnaji3@uwo.ca>
2020-01-08 16:11:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -12,25 +12,3 @@ input.addEventListener('keydown', function(e) {
   console.log(e.key);
 });
 
-const input = document.querySelector('input')
-
-input.addEventListener('keydown', function(e) {
-  if (e.key === "g") {
-    return e.preventDefault()
-  } else {
-    console.log(e.key)
-  }
-});
-
-const divs = document.querySelectorAll('div');
-
-function bubble(e) {
-  // stop! that! propagation!
-  e.stopPropagation();
-
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
-}
-
-for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener('click', bubble);
-}
